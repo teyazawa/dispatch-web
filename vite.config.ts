a874d454b4d20abfea5f050a89ea6c2f368be946
Showta Yazawa <teyazawa@users.noreply.github.com>
2026-01-14 16:01:48 +0900

    voicepanel html2

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,8 +8,8 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: './index.html',
-        voicePanel: './voice-panel.html',
+        main: path.resolve(__dirname, 'index.html'),
+        voicePanel: path.resolve(__dirname, 'public/voice-panel.html'),
       },
     },
   },
